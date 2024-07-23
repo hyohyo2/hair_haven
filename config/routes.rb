2c6b8745400586238627728d2e20986652c40a45
hyohyo2 <hono.handball.0910@gmail.com>
2024-07-23 07:17:51 +0000

    [Add]コントローラ追加(search以外)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
 # 顧客用
 # URL /users/sign_in ...
   devise_for :users,skip: [:passwords], controllers: {
@@ -17,5 +18,76 @@ Rails.application.routes.draw do
 # 顧客用
   root to: 'public/homes#top'
   get '/about' => 'public/homes#about', as: 'about'
+  
 
+
+
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'notice_genres/index'
+    get 'notice_genres/edit'
+  end
+  namespace :admin do
+    get 'notices/new'
+    get 'notices/index'
+    get 'notices/show'
+    get 'notices/edit'
+  end
+  namespace :admin do
+    get 'tags/index'
+    get 'tags/edit'
+  end
+  namespace :admin do
+    get 'reviews/index'
+  end
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    get 'items/new'
+    get 'items/index'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'tags/index'
+  end
+  namespace :public do
+    get 'reviews/new'
+    get 'reviews/index'
+  end
+  namespace :public do
+    get 'notices/index'
+    get 'notices/show'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/thanks'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/unsubscribe'
+    get 'users/favorites'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
 end
---
+++ test/controllers/public/notices_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class Public::NoticesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get public_notices_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get public_notices_show_url
+    assert_response :success
+  end
+end
