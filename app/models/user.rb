7a56f098f27130854ed242724f8234e36c0eef7e
hyohyo2 <hono.handball.0910@gmail.com>
2024-07-25 15:20:53 +0000

    [Add]バリデーション追加/userモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum gender: { genderless: 0, female: 1, male: 2 }
-  
-  
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :gender, presence: true
+  validates :post_code, presence: true, length: { is: 7 }
+  validates :address, presence: true
+  validates :telephone_number, presence: true, length: { in: 10..11 }
+  validates :is_active, inclusion: { in: [true,false] }
+
 
 end
