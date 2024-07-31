a08d6e78deef124a91be804678c5ff83610880af
hyohyo2 <hono.handball.0910@gmail.com>
2024-07-31 09:05:38 +0000

    [Add]会員情報編集ページ

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,11 +7,14 @@ class Public::UsersController < ApplicationController
 
   # 会員情報編集
   def edit
+    @user = current_user
   end
 
   # 会員情報の更新処理
   def update
-
+    @user = current_user
+    @user.update(user_params)
+    redirect_to mypage_path
   end
 
   # 退会確認画面
@@ -25,6 +28,12 @@ class Public::UsersController < ApplicationController
 
   # いいねした商品一覧
   def favorites
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :gender, :post_code, :address, :telephone_number, :email)
+
   end
 
 end
