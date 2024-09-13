a397c79b51626291bdbf6b394865cd46215e9bf8
hyohyo2 <hono.handball.0910@gmail.com>
2024-09-13 02:40:39 +0000

    [Fix]フラッシュメッセージ、カート遷移先

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -10,4 +10,9 @@ class Public::ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active)
+  end
 end
