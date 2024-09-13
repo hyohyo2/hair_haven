b3b53eb2fcfb53311f2934859a853497831a309b
hyohyo2 <hono.handball.0910@gmail.com>
2024-09-13 02:42:24 +0000

    [delete]item_params

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -10,9 +10,4 @@ class Public::ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
-
-  private
-  def item_params
-    params.require(:item).permit(:name, :introduction, :price, :is_active)
-  end
 end
