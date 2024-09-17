cc6b404dfda67f134d3625ff46e60f5f7d53a43c
hyohyo2 <hono.handball.0910@gmail.com>
2024-09-17 03:34:52 +0000

    [Add]注文確認支払方法・届け先追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,13 +8,13 @@ class Public::OrdersController < ApplicationController
 
   # 注文情報確認
   def confirm
-    @order = Order.new
+    @order = Order.new(order_params)
     if params[:order][:select_address] == "0"
       @order.post_code = current_user.post_code
       @order.address = current_user.address
       @order.name = current_user.full_name
     elsif params[:order][:select_address] == "1"
-      select = Address.find(params[:order][:user_id])
+      select = Address.find(params[:order][:address_id])
       @order.post_code = select.post_code
       @order.address = select.address
       @order.name = select.name
@@ -25,13 +25,14 @@ class Public::OrdersController < ApplicationController
     else
       render :new
     end
-    @cart_items = CartItem.all
+    @cart_items = current_user.cart_items.all
     # カラムのshipping_costとモデルに定義したget_shipping_costの値を同じにする
     @order.shipping_cost = @order.get_shipping_cost
   end
 
   # 注文確定処理
   def create
+    
 
   end
 
@@ -45,6 +46,12 @@ class Public::OrdersController < ApplicationController
 
   # 注文履歴詳細
   def show
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:user_id, :post_code, :address, :name, :shipping_cost, :total_payment, :payment_method)
   end
 
 
