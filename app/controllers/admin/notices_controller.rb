a7f944ca222e70035cf0e8492f8db34dbf85777a
hyohyo2 <hono.handball.0910@gmail.com>
2024-09-24 04:13:46 +0000

    [Add]管理者お知らせ機能追加

--- app/controllers/admin/notices_controller.rb
+++ app/controllers/admin/notices_controller.rb
@@ -3,33 +3,65 @@ class Admin::NoticesController < ApplicationController
 
   # お知らせ登録フォーム
   def new
+    @notice = Notice.new
   end
 
   # お知らせデータ登録処理
   def create
-
+    @notice = Notice.new(notice_params)
+    if @notice.save
+      flash[:notice] = "お知らせを登録しました。"
+      redirect_to admin_notice_path(@notice.id)
+    else
+      flash.now[:alert] = "お知らせの登録に失敗しました。"
+      render :new
+    end
   end
 
   # お知らせ一覧
   def index
+    @notices = Notice.all
   end
 
   # お知らせ詳細
   def show
+    @notice = Notice.find(params[:id])
   end
 
   # お知らせ編集フォーム
   def edit
+    @notice = Notice.find(params[:id])
   end
 
   # お知らせデータ更新処理
   def update
-
+    @notice = Notice.find(params[:id])
+    if @notice.update(notice_params)
+      flash[:notice] = "お知らせの内容を変更しました。"
+      redirect_to admin_notice_path(@notice)
+    else
+      flash.now[:alert] = "お知らせの内容の変更に失敗しました。"
+      render :edit
+    end
   end
 
   # お知らせデータ削除処理
   def destroy
+    @notice = Notice.find(params[:id])
+    if @notice.destroy
+      flash[:notice] = "お知らせを削除しました。"
+      redirect_to admin_notices_path
+    else
+      flash.now[:alert] = "お知らせの削除に失敗しました。"
+      @notice = Notice.find(params[:id])
+      render :show
+    end
+  end
 
+  private
+
+  def notice_params
+    params.require(:notice).permit(:title, :detail, :notice_genre_id)
   end
 
 end
--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,5 +1,9 @@
 class Notice < ApplicationRecord
-  
+
   belongs_to :notice_genre
-  
+
+  validates :title, presence: true
+  validates :detail, presence: true
+  validates :notice_genre_id, presence: true
+
 end
