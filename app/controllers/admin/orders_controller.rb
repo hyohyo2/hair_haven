24385e38c3cbb8eae37e5a7802db886a5ee8f256
hyohyo2 <hono.handball.0910@gmail.com>
2024-10-10 03:38:31 +0000

    [Fix]admin/ステータス選択追加修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,6 +9,17 @@ class Admin::OrdersController < ApplicationController
 
   # 注文データ更新処理
   def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.update(order_params)
+    if @order.order_status == "comfirmed_payment"
+      @order_details.update_all(making_status: 1)
+    end
+    redirect_to admin_order_path(@order)
+  end
 
+  private
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 end
